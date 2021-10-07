50cc4694b16f70ba579c8f6e93219946dabcd207
Kyle Musco <kmusco@ycp.edu>
2021-10-06 22:17:39 -0400

    Initial Commit

---
+++ FluentStream/FluentStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluentStreamApp.swift
+//  FluentStream
+//
+//  Created by Kyle Musco on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FluentStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
