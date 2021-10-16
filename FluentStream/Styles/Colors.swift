ed248dcedcb799b1ac5b5e848be7a9d8f9922e0d
Kyle Musco <kmusco@ycp.edu>
2021-10-16 11:37:51 -0400

    Gray message color is now defined as extension to Colors

--- FluentStream/Styles/Colors.swift
+++ FluentStream/Styles/Colors.swift
@@ -9,4 +9,5 @@ import SwiftUI
 
 extension Color {
     static let lightGray = Color(red: 237 / 255, green: 238 / 255, blue: 240 / 255)
+    static let grayMessage = Color(red: 240 / 255, green: 240 / 255, blue: 240 / 255)
 }
