8fb99a006e172462374d24e111e123ad13f4a3e1
Kyle Musco <kmusco@ycp.edu>
2021-10-19 16:54:15 -0400

    Update XCTests to support updates to ChatViewModel and Chat model

--- FluentStreamTests/FluentStreamTests.swift
+++ FluentStreamTests/FluentStreamTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import FluentStream
 
 class FluentStreamTests: XCTestCase {
-    let chatManager = ChatManager.shared
+    let chatManager = ChatViewModel()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,23 +21,23 @@ class FluentStreamTests: XCTestCase {
 
     func testSortMessagesIntoChats_1() {
         let messages = [
-            Message(id: 1, timestamp: "1", username_from: "test_user_1", username_to: "test_user_2", recording: "test.mp3"),
-            Message(id: 2, timestamp: "2", username_from: "test_user_1", username_to: "test_user_2", recording: "test.mp3"),
-            Message(id: 3, timestamp: "3", username_from: "test_user_1", username_to: "test_user_3", recording: "test.mp3")
+            Message(id: 1, timestamp: Date(timeIntervalSince1970: TimeInterval(1)), username_from: "test_user_1", username_to: "test_user_2", recording: "test.mp3"),
+            Message(id: 2, timestamp: Date(timeIntervalSince1970: TimeInterval(2)), username_from: "test_user_1", username_to: "test_user_2", recording: "test.mp3"),
+            Message(id: 3, timestamp: Date(timeIntervalSince1970: TimeInterval(3)), username_from: "test_user_1", username_to: "test_user_3", recording: "test.mp3")
         ]
         
-        let chats = chatManager.sortMessagesIntoChats(messages: messages)
+        let chats = chatManager.sortMessagesIntoChats(messages, true)
         
         XCTAssertEqual(chats.count, 2)
-        XCTAssertEqual(chats[0].dateOfLastMessage, "2")
-        XCTAssertEqual(chats[0].user1, "test_user_1")
-        XCTAssertEqual(chats[0].user2, "test_user_2")
-        XCTAssertEqual(chats[0].messages.count, 2)
+        XCTAssertEqual(chats[0].dateOfLastMessage, Date(timeIntervalSince1970: TimeInterval(3)))
+        XCTAssertEqual(chats[0].users[0], "test_user_1")
+        XCTAssertEqual(chats[0].users[1], "test_user_3")
+        XCTAssertEqual(chats[0].messages.count, 1)
         
-        XCTAssertEqual(chats[1].dateOfLastMessage, "3")
-        XCTAssertEqual(chats[1].user1, "test_user_1")
-        XCTAssertEqual(chats[1].user2, "test_user_3")
-        XCTAssertEqual(chats[1].messages.count, 1)
+        XCTAssertEqual(chats[1].dateOfLastMessage, Date(timeIntervalSince1970: TimeInterval(2)))
+        XCTAssertEqual(chats[1].users[0], "test_user_1")
+        XCTAssertEqual(chats[1].users[1], "test_user_2")
+        XCTAssertEqual(chats[1].messages.count, 2)
     }
 
 }
